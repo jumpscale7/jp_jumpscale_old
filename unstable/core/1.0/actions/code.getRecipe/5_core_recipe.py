65b516610d48b42bb5583d99a792e887bd4c7b94
Jo De Boeck <deboeck.jo@gmail.com>
2013-10-02 14:22:11 +0000

    Change bin files

--- unstable/core/1.0/actions/code.getRecipe/5_core_recipe.py
+++ unstable/core/1.0/actions/code.getRecipe/5_core_recipe.py
@@ -3,7 +3,7 @@ def main(j, args, params, tags, tasklet):
     repo=j.clients.bitbucket.getRepoConnection("jumpscale","jumpscale_core")
 
     recipe.add(repo, "lib/JumpScale", "$(python.paths.local.sitepackages)/JumpScale")
-    recipe.add(repo, "shellcmds", "$(bin.local")
+    recipe.add(repo, "shellcmds", "$(bin.local)")
 
     params.result = recipe  # remember for further usage
 
