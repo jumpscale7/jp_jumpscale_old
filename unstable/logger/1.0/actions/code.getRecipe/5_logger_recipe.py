fddd2b681a24c3befef5448428afaadfca576842
Jo De Boeck <deboeck.jo@gmail.com>
2013-10-03 16:10:38 +0200

    Logger should be platform generic

--- unstable/logger/1.0/actions/code.getRecipe/5_logger_recipe.py
+++ unstable/logger/1.0/actions/code.getRecipe/5_logger_recipe.py
@@ -1,7 +1,7 @@
 def main(j, args, params, tags, tasklet):
     recipe = j.packages.getCodeManagementRecipe()
     repo=j.clients.bitbucket.getRepoConnection("jumpscale","jumpscale_grid")
-    recipe.add(repo,"apps/logger","apps/logger")
+    recipe.add(repo,"apps/logger","apps/logger", platform='generic')
 
     params.result = recipe
 
