abdf29a91ce3f5ee72e44f605699b0dbaf140742
despiegk <despiegk@users.noreply.github.com>
2013-10-11 17:40:17 +0200

    package

--- unstable/libs/1.0/actions/code.package/5_libs_package.py
+++ unstable/libs/1.0/actions/code.package/5_libs_package.py
@@ -6,7 +6,7 @@ def main(j,args,params,tags,tasklet):
 
     recipe=qp.actions.code_getRecipe()
 
-    recipe.package(qp, "linux64")
+    recipe.package(qp, "generic")
     #this is the standard used package function, can overrule to do custom work
     
     return params
