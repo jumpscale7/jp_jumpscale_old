f0d9c0cca5242061c566cac712c1b4d78e3660c9
despiegk <despiegk@users.noreply.github.com>
2013-10-12 07:56:59 +0200

    commit missing jpackage files, addremove

---
+++ unstable/openvpn/2.3/actions/install/5_openvpn_install.py
@@ -0,0 +1,26 @@
+def main(j,args,params,tags,tasklet):
+   
+    #install the required files onto the system
+
+    # can happen by e.g. installing a debian package e.g. by
+    j.system.platform.ubuntu.install("openvpn")
+       
+    ##args.qp.copyFiles(subdir="root",destination="/",applyhrd=True) #  will copy files from subdir called root of platforms to root of system (carefull), will also use templateEngine for hrd 
+
+    ##copy python libs: then need to be in subdir site-packages of one of the platforms, they will all be copied to site-packages in local python dir
+    #args.qp.copyPythonLibs(remove=False)
+
+    #install found debs they need to be in debs dir of one or more of the platforms (all relevant platforms will be used)
+    #args.qp.installUbuntuDebs()
+    
+    #shortcut to some usefull install tools
+    #do=j.system.installtools
+
+    #configuration is not done in this step !!!!!
+    
+    params.result=True #return True if result ok
+    return params
+    
+    
+def match(j,args,params,tags,tasklet):
+    return True
