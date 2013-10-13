189bd2182b3976c704ac727e6ecc32a0e1d8cc4a
Jo De Boeck <deboeck.jo@gmail.com>
2013-10-13 11:18:42 +0200

    Mod in configure tasklets of elasticsearch and circus

--- unstable/circus/1.0/actions/configure/5_startupmanager_configure.py
+++ unstable/circus/1.0/actions/configure/5_startupmanager_configure.py
@@ -3,13 +3,14 @@ def main(j,args,params,tags,tasklet):
     #configure the package
     cfgpath = j.system.fs.joinPaths(j.dirs.cfgDir, 'startup')
     j.system.fs.createDir(cfgpath)
-    config = """[circus]
-check_delay = 5
-"""
-    j.system.fs.writeFile(j.system.fs.joinPaths(cfgpath, 'server.ini'), config)
-    j.system.installtools.execute("circusd --daemon %s" % j.system.fs.joinPaths(cfgpath, 'server.ini'))
-    
+    cfg = j.system.fs.joinPaths(cfgpath, 'server.ini')
+    ini = j.tools.inifile.open(cfg)
+    ini.addSection('circus')
+    ini.addParam('circus', 'include_dir', cfgpath)
     j.system.platform.ubuntu.serviceInstall('circus', '/usr/local/bin/circusd', '--daemon %s' % j.system.fs.joinPaths(cfgpath, 'server.ini'))
+    j.system.platform.ubuntu.startService('circus')
+    import JumpScale.baselib.circus
+    j.tools.circus.manager.apply()
 
     return params
     
