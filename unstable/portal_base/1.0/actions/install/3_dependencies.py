89927d93e6f071dee88c3c6c72d3775a37d6274e
Sherif Zain <sherifzain@yahoo.com>
2013-10-27 13:48:14 +0200

    Added dependencies to portalbase

--- unstable/portal_base/1.0/actions/install/3_dependencies.py
+++ unstable/portal_base/1.0/actions/install/3_dependencies.py
@@ -5,6 +5,8 @@ def main(j, args, params, tags, tasklet):
     j.system.platform.ubuntu.install('nginx-full')
     j.system.platform.ubuntu.install('python-beaker')
     j.system.platform.ubuntu.install('python-mimeparse')
+    j.system.platform.ubuntu.install('redis-server')
+    j.system.platform.ubuntu.install('python-redis')
 
     params.result = True  # return True if result ok
     return params
