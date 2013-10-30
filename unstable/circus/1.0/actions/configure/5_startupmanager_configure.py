d1d882a816259a062499a91b3cc4331ad4ec2249
Sherif Zain <sherifzain@yahoo.com>
2013-10-30 22:00:52 +0200

    Cleanup code + changed circus socket polling timeout + Added log output

--- unstable/circus/1.0/actions/configure/5_startupmanager_configure.py
+++ unstable/circus/1.0/actions/configure/5_startupmanager_configure.py
@@ -1,6 +1,5 @@
 def main(j,args,params,tags,tasklet):
-   
-    #configure the package
+
     cfgpath = j.system.fs.joinPaths(j.dirs.cfgDir, 'startup')
     j.system.fs.createDir(cfgpath)
 
@@ -9,7 +8,7 @@ def main(j,args,params,tags,tasklet):
     ini = j.tools.inifile.open(cfg)
     ini.addSection('circus')
     ini.addParam('circus', 'include', "*.ini")
-    ini.addParam('circus','check_delay',5)
+    ini.addParam('circus','check_delay', 30)
     ini.addParam('circus','httpd','True')
     ini.addParam('circus','httpd_host','localhost')
     ini.addParam('circus','httpd_port',8080)
@@ -17,15 +16,11 @@ def main(j,args,params,tags,tasklet):
 
     
     j.system.platform.ubuntu.serviceInstall('circus', '/usr/local/bin/circusd', '--daemon %s' % j.system.fs.joinPaths(cfgpath, 'server.ini'))
-
-    # j.system.installtools.execute("circusd --daemon %s" % j.system.fs.joinPaths(cfgpath, 'server.ini'))    
     j.system.platform.ubuntu.serviceUninstall('circus')
     for port in [5555,5556,5557,8080]:
         j.system.process.killProcessByPort(port)
     
-    cmd="python /usr/local/lib/python2.7/site-packages/circus/circusd.py --log-level info /opt/jumpscale/cfg/startup/server.ini "
-    # cmd='/usr/local/bin/circusd'
-    # args='%s' % j.system.fs.joinPaths(cfgpath, 'server.ini')
+    cmd="python /usr/local/lib/python2.7/site-packages/circus/circusd.py --log-output /var/log/circus.log --log-level info /opt/jumpscale/cfg/startup/server.ini "
     j.system.platform.ubuntu.serviceInstall('circus', cmd, "")
     j.system.platform.ubuntu.startService('circus')
 
