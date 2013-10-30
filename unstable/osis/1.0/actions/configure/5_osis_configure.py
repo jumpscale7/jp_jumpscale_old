e0d15cda0399bcf36868e944b08791d0a1fb3004
Sherif Zain <sherifzain@yahoo.com>
2013-10-30 16:28:56 +0200

    Added support to start osis via circus

--- unstable/osis/1.0/actions/configure/5_osis_configure.py
+++ unstable/osis/1.0/actions/configure/5_osis_configure.py
@@ -1,9 +1,17 @@
 def main(j,args,params,tags,tasklet):
-   
-    #configure the package 
+    import JumpScale.baselib.circus
 
+    cmd = 'python'
+    args = 'osisServerStart.py'
+    workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'osis')
+    j.tools.circus.manager.addProcess('osis', cmd, args, priority=2, workingdir=workingdir, before_start='JumpScale.baselib.circus.CircusManager.checkPort')
+    env_vars = {'WAIT_FOR_PORT': 9200}
+    j.tools.circus.manager.addEnv('osis', env_vars)
+    j.tools.circus.manager.apply()
+    j.tools.circus.manager.startProcess('osis')
+
+    params.result = True
     return params
-    
-    
+
 def match(j,args,params,tags,tasklet):
     return True
