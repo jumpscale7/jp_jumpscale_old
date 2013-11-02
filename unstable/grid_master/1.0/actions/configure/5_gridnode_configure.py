e50752e89420f3141a249def721128912fe77da9
despiegk <despiegk@users.noreply.github.com>
2013-11-02 10:11:45 +0100

    commit missing jpackage files, addremove

--- unstable/grid_master/1.0/actions/configure/5_gridnode_configure.py
+++ unstable/grid_master/1.0/actions/configure/5_gridnode_configure.py
@@ -2,35 +2,20 @@ def main(j,args,params,tags,tasklet):
    
     #configure the package 
 
+    gridid=j.application.config.getInt("gridmaster.grid.id")
+
     master_exists=False
-    if j.application.config.getBool("grid.useavahi"):
+    if j.application.config.getBool("gridmaster.useavahi"):
         import JumpScale.lib.remote.avahi
         s=j.remote.avahi.getServices()
-        master_exists,services=s.exists(partofname="jsgrid.1.master")
+        master_exists,services=s.exists(partofname="jsgrid.%s.master"%gridid)
         if master_exists:
             from IPython import embed
             print "DEBUG NOW master exist"
             embed()
             
             masterip=1
-        master_exists=True
-
-    if master_exists==False:
-        if j.application.config.get("grid.ismaster")=="":
-            ismaster=j.console.askYesNo("Is this node the master of the grid?")
-            if ismaster:
-                if j.console.askYesNo("Do you want to enable this node to become the master of the grid?"):
-                    
-                    from IPython import embed
-                    print "DEBUG NOW question master grid"
-                    embed()
-            
-        else:
-            pass 
-            #from IPython import embed
-            #print "DEBUG NOW configuregridnode"
-            #embed()
-    
+        master_exists=True    
 
     return params
     
--- unstable/logger/1.0/actions/code.getRecipe/5_logger_recipe.py
+++ unstable/logger/1.0/actions/code.getRecipe/5_logger_recipe.py
@@ -1,12 +1,19 @@
-def main(j, args, params, tags, tasklet):
-    recipe = j.packages.getCodeManagementRecipe()
+def main(j,args,params,tags,tasklet):
+   
+    #create codemgmt recipe
+    #a codemanagement recipe is used to define where the code from the code repo needs to go to
+    
+    recipe=j.packages.getCodeManagementRecipe()
+    
     repo=j.clients.bitbucket.getRepoConnection("jumpscale","jumpscale_grid")
-    recipe.add(repo,"apps/logger","apps/logger", platform='generic')
-
-    params.result = recipe
-
+    
+    #recipe.add(repo, sourcePath, destinationPath,type=None, branch='')  #when type="config" then this entry will not be linked when doing codelink but will be copied because is config info
+    recipe.add(repo,"apps/logger","apps/logger",type="config")
+    
+    params.result=recipe  #remember for further usage
+    
     return params
-
-
-def match(j, args, params, tags, tasklet):
+    
+    
+def match(j,args,params,tags,tasklet):
     return True
