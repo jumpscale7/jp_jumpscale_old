8d7c40b0a083edb70fd710d4b33423345511f319
JumpScale Jenkins <jenkins@jumpscale.com>
2013-11-03 10:35:12 +0000

    Renamed grid_node to gridnode

---
+++ unstable/gridnode/1.0/actions/code.link/5_gridnode_link.py
@@ -0,0 +1,16 @@
+def main(j,args,params,tags,tasklet):
+   
+    #link info into local repo
+
+    jp=args.jp
+
+    recipe=jp.actions.code_getRecipe()
+
+    recipe.link()
+    #this is the standard used function, can overrule to do custom work
+    
+    return params
+    
+    
+def match(j,args,params,tags,tasklet):
+    return True
