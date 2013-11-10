fff434a802f4805054fe96b763bd5eccd94bbeaa
Jo De Boeck <deboeck.jo@gmail.com>
2013-11-10 15:20:12 +0200

    Refactor recipes

--- unstable/jumpscale_examples/1.0/actions/install/5_jumpscale_examples_install.py
+++ unstable/jumpscale_examples/1.0/actions/install/5_jumpscale_examples_install.py
@@ -1,6 +1,5 @@
 def main(j, args, params, tags, tasklet):
 
-    args.jp.copyFiles(subdir="apps", destination="%s/apps" % j.dirs.baseDir)
     params.result = True  # return True if result ok
     return params
 
