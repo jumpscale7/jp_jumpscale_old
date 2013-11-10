66fbb77ab28948a3b6414c58dcc23593e5545aab
Jo De Boeck <deboeck.jo@gmail.com>
2013-11-10 15:35:45 +0200

    More recipe refactoring

--- unstable/nxfree_server/3.5/actions/install/5_nxfree_server_install.py
+++ unstable/nxfree_server/3.5/actions/install/5_nxfree_server_install.py
@@ -1,32 +1,5 @@
 def main(j,args,params,tags,tasklet):
    
-    #install the required files onto the system
-    # can happen by e.g. installing a debian package e.g. by
-    ## j.system.platform.ubuntu.install(packagename)
-    # can happen by copying files from the jpackages included files (they come from the bundle) e.g. by
-    
-    #args.jp.copyFiles() #  will copy files to sandbox
-
-
-    path = args.jp.getPathFilesPlatform('linux64')
-
-    packages = ('nxclient_3.5.0-7_amd64.deb',
-                'nxnode_3.5.0-9_amd64.deb',
-                'nxserver_3.5.0-11_amd64.deb')
-
-    for package in packages:
-        j.system.platform.ubuntu.installDebFile(j.system.fs.joinPaths(path, package))
-
-    
-    ##params.jpackages.copyFiles(destination="/opt/qbase3") #  will copy files to sandbox qbase3
-    
-
-    te= j.codetools.getTextFileEditor("/usr/NX/etc/server.cfg")
-    te.replace1Line("EnableAdministratorLogin = \"1\"",["EnableAdministratorLogin*"])
-    te.save()
-
-    j.system.installtools.execute("/etc/init.d/nxserver restart")
-
     #configuration is not done in this step !!!!!
     
     params.result=True #return True if result ok
--- unstable/shorewall/4.5/actions/install/5_shorewall_install.py
+++ unstable/shorewall/4.5/actions/install/5_shorewall_install.py
@@ -1,13 +1,5 @@
 def main(j,args,params,tags,tasklet):
    
-    #install the required files onto the system
-
-    # can happen by e.g. installing a debian package e.g. by
-    j.system.platform.ubuntu.install("shorewall")
-       
-
-    #configuration is not done in this step !!!!!
-    
     params.result=True #return True if result ok
     return params
     
