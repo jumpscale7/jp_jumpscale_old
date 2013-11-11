aa6a056f27f03100b4af800556fd06461888e090
Jo De Boeck <deboeck.jo@gmail.com>
2013-11-11 17:07:27 +0200

    Make sure to remove pip JumpScale when install JumpScale core

--- unstable/core/1.0/actions/prepare/5_core_prepare.py
+++ unstable/core/1.0/actions/prepare/5_core_prepare.py
@@ -1,19 +1,6 @@
 def main(j,args,params,tags,tasklet):
    
-    #install the required files onto the system
-
-    # can happen by e.g. installing a debian package e.g. by
-    ## j.system.platform.ubuntu.install(packagename)
-       
-    #install found debs they need to be in debs dir of one or more of the platforms (all relevant platforms will be used)
-    #args.qp.installUbuntuDebs()
-    
-    #shortcut to some usefull install tools
-    #do=j.system.installtools
-
-    #configuration is not done in this step !!!!!
-    #copying files from files section of jpackages is not done in this step
-    
+    args.jp.uninstall() 
     params.result=True #return True if result ok
     return params
     
