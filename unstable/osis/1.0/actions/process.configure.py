180957ff4bcb6b0e267961037c2b0c8eaa1e4299
despiegk <despiegk@users.noreply.github.com>
2013-11-12 08:51:07 +0100

    configure

--- unstable/osis/1.0/actions/process.configure.py
+++ unstable/osis/1.0/actions/process.configure.py
@@ -1,15 +1,12 @@
 def main(jp):
    
     #configure the application to autostart
-    
-    # jp.log("set autostart $(jp.name)")
+    jp.log("set autostart $(jp.name)")
 
     # #example start osis
-    # cmd = 'python'
-    # args = 'osisServerStart.py'
-    # workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'osis')
-    # j.tools.startupmanager.addProcess(name, cmd, args=args, env={}, numprocesses=1, priority=1, shell=False, workingdir=workingdir,jpackage=jp,domain="",ports=[])
+    cmd = 'python'
+    args = 'osisServerStart.py'
+    workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'osis')
+
+    j.tools.startupmanager.addProcess("osis", cmd, args=args, env={}, numprocesses=1, priority=1, shell=False, workingdir=workingdir,jpackage=jp,domain="",ports=[5544])
     
-
-
-    pass
