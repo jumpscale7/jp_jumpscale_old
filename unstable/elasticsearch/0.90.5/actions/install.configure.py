f3615f4e8a01db2409229c8c0c0af80978edb605
Reem Khamis <rkhamis@users.noreply.github.com>
2013-11-12 15:03:49 +0200

    Merging fixes

--- unstable/elasticsearch/0.90.5/actions/install.configure.py
+++ unstable/elasticsearch/0.90.5/actions/install.configure.py
@@ -1,8 +1,5 @@
 from JumpScale import j
 def main(jp):
-<<<<<<< local
-    pass
-=======
     import JumpScale.baselib.startupmanager
     #make sure there are no traces of elastic search any more
     j.system.process.killProcessByPort(9200)
@@ -11,4 +8,3 @@ def main(jp):
 
     j.tools.startupmanager.addProcess(name="elasticsearch", cmd=cmd, args=args, priority=1)
     j.tools.startupmanager.apply()
->>>>>>> other
--- unstable/osis/1.0/actions/install.configure.py
+++ unstable/osis/1.0/actions/install.configure.py
@@ -1,17 +1,10 @@
 from JumpScale import j
 def main(jp):
-<<<<<<< local
-=======
     import JumpScale.baselib.startupmanager
     cmd = 'python'
     args2 = 'osisServerStart.py'
->>>>>>> other
-
-<<<<<<< local
-    pass=======
     jp.log("set autostart")
     
     workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'osis')
 
     j.tools.startupmanager.addProcess('osis', cmd, args2, priority=2, workingdir=workingdir,jpackage=jp)
->>>>>>> other
