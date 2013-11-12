3c3b4465cebda318a07185b784ca35f35bcf053e
Jo De Boeck <deboeck.jo@gmail.com>
2013-11-12 11:16:31 +0200

    Remove stuf that doesnt work anymore

--- unstable/osis/1.0/actions/install.configure.py
+++ unstable/osis/1.0/actions/install.configure.py
@@ -9,6 +9,3 @@ def main(jp):
     workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'osis')
 
     j.tools.startupmanager.addProcess('osis', cmd, args2, priority=2, workingdir=workingdir,jpackage=jp)
-
-    env_vars = {'WAIT_FOR_PORT': 9200}
-    j.tools.startupmanager.addEnv('osis', env_vars)
