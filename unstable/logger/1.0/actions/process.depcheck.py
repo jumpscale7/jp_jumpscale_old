d7488a92a454aa755106cc1838b92507328b1167
JumpScale Jenkins <jenkins@jumpscale.com>
2013-11-24 07:53:46 +0000

    Jenkins autobuild for portal  jumpscale

--- unstable/logger/1.0/actions/process.depcheck.py
+++ unstable/logger/1.0/actions/process.depcheck.py
@@ -1,14 +1,8 @@
 def main(j,jp):
    
-    #check can access osis
+    #check dependencies for process to start e.g. can I reach local or remote app
 
-    print "test if osis is reachable by doing a port test"
+    #e.g.
+    #return j.system.net.tcpPortConnectionTest(ipaddr, 9200)
 
-    masterip=j.application.config.get("grid.master.ip")
-
-    if j.system.net.waitConnectionTest(masterip,5544,2)==False:
-        raise RuntimeError("Could not configure logger, osis was not found to be active on %s on port 5544."%masterip)       
-
-    print "osis reachable" 
-
-    return True
+    pass
