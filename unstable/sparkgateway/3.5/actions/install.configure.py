0f6a4ee4fdd1e7343b41ad5e43fe8bf45113daaf
Jo De Boeck <deboeck.jo@gmail.com>
2013-11-25 16:53:18 +0200

    Add autostart for sparkgateway + configuration of port

--- unstable/sparkgateway/3.5/actions/install.configure.py
+++ unstable/sparkgateway/3.5/actions/install.configure.py
@@ -1,6 +1,5 @@
 def main(j,jp):
-   
-    #configure the package 
-
-    pass
-    
+    cfgpath = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'SparkGateway', 'gateway.conf') 
+    te = j.codetools.getTextFileEditor(cfgpath)
+    te.appendReplaceLine('^\s*port\s*=', 'port = $(sparkgateway.port)')
+    te.save()
