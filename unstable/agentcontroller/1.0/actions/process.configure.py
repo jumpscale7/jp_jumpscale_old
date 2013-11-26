47a80cdff1e30d5d5fc42c1db423f48c068b10a5
Reem Khamis <rkhamis@users.noreply.github.com>
2013-11-26 10:55:30 +0200

    Changing priorities to prevent race conditions
    Reviewer: Sherif Zain

--- unstable/agentcontroller/1.0/actions/process.configure.py
+++ unstable/agentcontroller/1.0/actions/process.configure.py
@@ -1,20 +1,16 @@
 def main(j,jp):
-   
     #configure the application to autostart
-    
     # jp.log("set autostart $(jp.name)")
 
-    # #example start osis
     cmd = 'python'
     args = 'controller.py'
     workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'agentcontroller')
     name="agentcontroller"
     j.tools.startupmanager.addProcess(name, cmd, args=args, env={}, numprocesses=1, priority=1, shell=False, workingdir=workingdir,jpackage=jp,domain="",ports=[4444])
-    
+
     for i in range($(agent.nrinstances)):
         cmd = 'python'
         args = 'agent.py'
         name="agent_%s"%i
         workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'agentcontroller')
-        j.tools.startupmanager.addProcess(name, cmd, args=args, env={}, numprocesses=1, priority=1, shell=False, workingdir=workingdir,jpackage=jp,domain="",ports=[])
-        
+        j.tools.startupmanager.addProcess(name, cmd, args=args, env={}, numprocesses=1, priority=5, shell=False, workingdir=workingdir,jpackage=jp,domain="",ports=[4444])
