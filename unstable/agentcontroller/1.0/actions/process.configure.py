e0305542d3bacb7894b2768f5168398d9f7f7a97
kristof <kristof@incubaid.com>
2013-11-30 08:25:15 -0500

    add remove missing files for

--- unstable/agentcontroller/1.0/actions/process.configure.py
+++ unstable/agentcontroller/1.0/actions/process.configure.py
@@ -8,9 +8,3 @@ def main(j,jp):
     name="agentcontroller"
     j.tools.startupmanager.addProcess(name, cmd, args=args, env={}, numprocesses=1, priority=20, shell=False, workingdir=workingdir,jpackage=jp,domain="",ports=[4444])
 
-    for i in range(j.basetype.integer.fromString('$(agent.nrinstances)')):
-        cmd = 'python'
-        args = 'agent.py'
-        name="agent_%s"%i
-        workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'agentcontroller')
-        j.tools.startupmanager.addProcess(name, cmd, args=args, env={}, numprocesses=1, priority=99, shell=False, workingdir=workingdir,jpackage=jp,domain="",ports=[4444])
