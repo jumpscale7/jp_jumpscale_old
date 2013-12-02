b8bfefe42415f8bf4efe438f1bac368c605e4dfb
kristof <kristof@incubaid.com>
2013-12-02 15:43:27 +0100

    commit missing jpackage files, addremove

--- unstable/grid_node/1.0/actions/install.configure.py
+++ unstable/grid_node/1.0/actions/install.configure.py
@@ -22,21 +22,31 @@ def main(j,jp):
         
         raise RuntimeError("Could not find ip addr of master, is master osis running? We did connection test on port 5544, but failed")
 
+    print "found master ip from avahi"
 
     #remember master ip for further usage
     j.application.config.set("grid.master.ip",masterip)
 
     #now register node
     import JumpScale.grid.osis
+    print "make connection to master osis"
     client = j.core.osis.getClient(masterip)
+    print "client ok"
     client_node=j.core.osis.getClientForCategory(client,"system","node")
+    print "client for client 'node' ok"
     client_grid=j.core.osis.getClientForCategory(client,"system","grid")
+    print "client for client 'grid' ok"
+    print "connection done, osis ok."
 
     obj = client_node.new()
     obj.initFromLocalNodeInfo()
     key, new, changed = client_node.set(obj)
+    print "save to osis"
 
     obj=client_node.get(key)
 
     j.application.config.set("grid.node.id",obj.id)
     j.application.config.set("grid.node.machineguid",obj.machineguid)
+
+    print "save to local hrd"
+    print "initialization of node done"
