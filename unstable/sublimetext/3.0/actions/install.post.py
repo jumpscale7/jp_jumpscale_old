4e9ee1589cde6a5fb00afee2ba2e15f1be196900
kristof <kristof@incubaid.com>
2013-12-06 16:00:50 +0200

    add remove missing files for

--- unstable/sublimetext/3.0/actions/install.post.py
+++
@@ -1,6 +0,0 @@
-
-def main(j,jp):
-
-    j.system.fs.remove("/usr/bin/sublime_text")
-
-    j.system.fs.symlink(path="/opt/sublimetext/sublime_text", target="/usr/bin/sublime_text", overwriteTarget=True)
--- unstable/sublimetext/3.0/actions/install.prepare.py
+++
@@ -1,4 +0,0 @@
-
-def main(j,jp):
-    j.system.platformtype.dieIfNotPlatform("linux64")
-    j.system.process.killProcessByName("sublimetext")
