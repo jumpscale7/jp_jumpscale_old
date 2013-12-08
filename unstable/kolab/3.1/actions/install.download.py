ec77e61933ee5dc9d288d6fcc1acd8f6029382f2
Jo De Boeck <deboeck.jo@gmail.com>
2013-12-08 08:32:31 +0100

    New build of portal + some template changes?

--- unstable/kolab/3.1/actions/install.download.py
+++ unstable/kolab/3.1/actions/install.download.py
@@ -1,5 +1,4 @@
-def main(j,jp,destination=None,force=False):
+def main(j,jp,destination=None,force=False,expand=True,nocode=False):
    
     #copying of files is done in this step
-    #jp._download(force=force,destination=destination)
-    pass
+    jp._download(force=force,destination=destination,expand=expand,nocode=nocode)
