0769da48b06329c000c86cfd4622a327d1f1b595
Jo De Boeck <deboeck.jo@gmail.com>
2013-12-12 19:23:41 +0200

    Remove php as requirement

--- unstable/portal/1.0/actions/install.prepare.py
+++ unstable/portal/1.0/actions/install.prepare.py
@@ -1,10 +1,5 @@
 
 def main(j,jp):
-    j.system.platform.ubuntu.install('php5-cgi')
-    try:
-        j.system.platform.ubuntu.install('php5-json')
-    except:
-        pass #needed on systems where it exists on other systems this is bundled with php5
     j.system.platform.ubuntu.install('python-imaging')
     j.system.platform.ubuntu.install('nginx-full')
     j.system.platform.ubuntu.install('python-beaker')
