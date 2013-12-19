a9c2f1b4cc88757d45c7815f951027dc4b0f0972
kristof <kristof@incubaid.com>
2013-12-19 11:13:22 +0100

    add remove missing files for

--- unstable/grid_portal/1.0/actions/code.link.py
+++ unstable/grid_portal/1.0/actions/code.link.py
@@ -2,6 +2,7 @@
 #if customizations are required when doing a a linking of the jpackage code to the OS
 
 def main(j,jp,force=True):
+    j.system.fs.removeDirTree("/opt/jumpscale/apps/gridportal")
     recipe=jp.getCodeMgmtRecipe()
     recipe.link(force=force)
 
