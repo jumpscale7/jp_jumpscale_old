0063359fdb879c746325315f9c4a8cba5c8057be
kristof <kristof@incubaid.com>
2013-12-28 08:48:23 +0100

    k

--- unstable/shorewall/4.5/actions/install.prepare.py
+++ unstable/shorewall/4.5/actions/install.prepare.py
@@ -2,7 +2,7 @@
 def main(j,jp):
     j.system.fs.removeDirTree("/etc/shorewall")
     j.system.platform.ubuntu.install("shorewall")
-    # j.system.platform.ubuntu.install("shorewall-lite")
+    j.system.platform.ubuntu.install("shorewall-lite")
 
     if not j.system.fs.exists(path="/var/log/messages"):
          j.system.fs.writeFile("/var/log/messages","")
