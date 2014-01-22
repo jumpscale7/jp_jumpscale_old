c47abe1d4eaa9729b7232e14feb7e21812440b67
kristof <kristof@incubaid.com>
2014-01-22 10:29:45 +0100

    processmanager integration with sentry

--- unstable/agent/1.0/actions/process.configure.py
+++ unstable/agent/1.0/actions/process.configure.py
@@ -3,23 +3,29 @@ def main(j,jp):
     # jp.log("set autostart $(jp.name)")
 
     # for i in range(j.basetype.integer.fromString('$(agent.nrinstances)')):
-    #     cmd = 'python'
-    #     args = 'agent.py'
-    #     name="agent_%s"%i
-    #     workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'agent')
-    #     j.tools.startupmanager.addProcess(name, cmd, args=args, env={}, numprocesses=1, priority=99, shell=False, workingdir=workingdir,jpackage=jp,domain="",ports=[])
+    cmd = 'python'
+    args = 'agent.py'
+    # name="agent_%s"%i
+    name="agent"
+    workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'agent')
+    j.tools.startupmanager.addProcess(name, cmd, args=args, env={}, numprocesses=1, priority=99, shell=False, workingdir=workingdir,jpackage=jp,domain="",ports=[])
 
 
     #e.g. default:2,hypervisor:1,io:1
     for q in "$(agent.queues)".split(","):
+        print "Queue:%s"%q
         splitted=q.split(":")
         if len(splitted)<>2:
             raise RuntimeError("format needs to be: default:2,hypervisor:1,io:1")
         qname,nrinstances=splitted
 
-        for nr in range(nrinstances):
+        for nr in range(int(nrinstances)):
             cmd = 'python'
             wname="worker_%s_%s"%(qname,nr)
-            args = 'worker.py -pw $(agent.redis.auth) -a $(agent.redis.addr) -p $(agent.redis.port) -qn %s -wn %s'%(qname,wname)            
+            if "$(agent.redis.auth)".strip()<>"":
+                args = 'worker.py -pw $(agent.redis.auth) -a $(agent.redis.addr) -p $(agent.redis.port) -qn %s -wn %s'%(qname,wname)
+            else:
+                args = 'worker.py -a $(agent.redis.addr) -p $(agent.redis.port) -qn %s -wn %s'%(qname,wname)
             workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'agent')
-            j.tools.startupmanager.addProcess(name, cmd, args=args, env={}, numprocesses=1, priority=99, shell=False, workingdir=workingdir,jpackage=jp,domain="",ports=[])
+            j.tools.startupmanager.addProcess(wname, cmd, args=args, env={}, numprocesses=1, priority=99, shell=False, \
+                workingdir=workingdir,jpackage=jp,domain="workers",ports=[])
--- unstable/grid_master/1.0/actions/install.configure.py
+++ unstable/grid_master/1.0/actions/install.configure.py
@@ -1,19 +1,27 @@
 
 def main(j,jp):
     gridid=j.application.config.getInt("gridmaster.grid.id")
-    j.application.config.set("grid.id",gridid)
 
+    j.application.config.set("grid.id",gridid)
     j.application.config.set("grid.master.ip","127.0.0.1")
 
     #check osis installed
     if not j.system.net.tcpPortConnectionTest("127.0.0.1", 5544):
         j.tools.startupmanager.startProcess('jumpscale', 'elasticsearch')
-        j.tools.startupmanager.startProcess('jumpscale', 'osis')
+        # j.tools.startupmanager.startProcess('jumpscale', 'osis')
+
+    if j.application.config.get("grid.master.superadminpasswd") == "":
+        passwdmd5 = j.tools.hash.md5_string(j.console.askString("Please provide: grid.master.superadminpasswd", regex="^(?!\s*$).+", retry=5))
+        j.application.config.set("grid.master.superadminpasswd",passwdmd5) #on gridmaster we should remember
+    
+    passwdmd5 = j.application.config.get("grid.master.superadminpasswd")
+
+    j.tools.startupmanager.restartProcess('jumpscale', 'osis')
+
 
     #register in osis
     import JumpScale.grid.osis
-    client = j.core.osis.getClient(user="root")
-    client.createNamespace(name="system",template="coreobjects",incrementName=False)
+    client = j.core.osis.getClient(user="root",passwd=passwdmd5)
     client_grid=j.core.osis.getClientForCategory(client,"system","grid")
 
     gridobj = client_grid.new(name="grid_%s"%gridid,id=gridid)
