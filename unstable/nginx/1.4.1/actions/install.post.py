8e365ab0aab2bf0cabc8804bd80059d428624bb2
Sherif Zain <sherifzain@yahoo.com>
2014-02-15 17:18:15 +0200

    Removed the default nginx server config, can be set later thru nginx extension

--- unstable/nginx/1.4.1/actions/install.post.py
+++ unstable/nginx/1.4.1/actions/install.post.py
@@ -1,15 +1,4 @@
 def main(j,jp):
-   
-    #copying of files is not done in this step
-    # the order is:
-    # first do prepare
-    # then the system automatically copies (if not in debug) starting from the files section of the jpackage
-    # then do this tasklet (postinstall)
-
-    #shortcut to some usefull install tools
-    #do=j.system.installtools
-
-    #configuration is not done in this step !!!!!
-    #preparation like system preps like ubuntu deb installs also not done here
-    
-    pass
+    # remove the default nginx server config
+    j.system.fs.remove(j.system.fs.joinPaths('/etc', 'nginx', 'sites-enabled', 'default'))
+    j.system.process.execute('service nginx restart')
