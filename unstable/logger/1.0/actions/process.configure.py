c3d82093880ded1f0b1e75c766c0dfe14a96aff8
Jo De Boeck <deboeck.jo@gmail.com>
2014-02-18 09:35:09 +0200

    Update logger to only add functionality to processmanager make processmanager run at startup

--- unstable/logger/1.0/actions/process.configure.py
+++ unstable/logger/1.0/actions/process.configure.py
@@ -1,6 +1,2 @@
 def main(j,jp):
     jp.log("set autostart $(jp.name)")
-    cmd = 'python'
-    args = 'loggerStart.py'
-    workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'logger')
-    j.tools.startupmanager.addProcess('logger', cmd, args=args, env={}, workingdir=workingdir,jpackage=jp,priority=30)
