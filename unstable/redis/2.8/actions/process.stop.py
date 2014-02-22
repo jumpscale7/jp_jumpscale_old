71201eff0832ed47cc370310b4ba25b759863881
kristof <kristof@incubaid.com>
2014-02-22 13:13:38 +0100

    first part of local workers rework

--- unstable/redis/2.8/actions/process.stop.py
+++ unstable/redis/2.8/actions/process.stop.py
@@ -2,15 +2,7 @@ def main(j,jp):
    
     #stop the application (only relevant for server apps)
     
-    jp.log("stop $(jp.name)")
-
-    if j.tools.startupmanager.existsJPackage(jp):
-        
-        j.tools.startupmanager.stopJPackage(jp)
-
-        for port in jp.tcpPorts:
-            j.system.process.killProcessByPort(port)
-
-        jp.waitDown(timeout=20)
-
+    import JumpScale.baselib.redis
+    j.clients.redis.stopInstance("redisp")
+    j.clients.redis.stopInstance("redisc")
 
