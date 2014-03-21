f23d6ef66db5926314579253a679c913c1b4df64
despiegk <despiegk@users.noreply.github.com>
2014-03-21 10:33:07 +0100

    new base DANGEROUS

--- unstable/base/2.7/actions/install.prepare.py
+++ unstable/base/2.7/actions/install.prepare.py
@@ -1,3 +1,5 @@
 
 def main(j,jp):
     j.system.fs.remove("/opt/jumpscale/cfg/hrd/python.hrd")
+    j.system.fs.createDir("/usr/local/lib/python2.7/dist-packages/")
+    j.system.fs.createDir("/usr/local/lib/python2.7/site-packages/")
