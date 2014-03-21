9fdde30ca4c88480b992c795b59c46e98a3cf280
despiegk <despiegk@users.noreply.github.com>
2014-03-21 10:45:05 +0100

    python lib mess fix

--- unstable/core/1.0/actions/install.prepare.py
+++ unstable/core/1.0/actions/install.prepare.py
@@ -2,7 +2,6 @@ def main(j,jp):
     j.system.fs.createDir("/usr/local/lib/python2.7/dist-packages/")
     j.system.fs.createDir("/usr/local/lib/python2.7/site-packages/")
     jp.uninstall() 
-    j.system.fs.createDir("/usr/local/lib/python2.7/dist-packages/")
-    j.system.fs.createDir("/usr/local/lib/python2.7/site-packages/")
+
 
     
--- unstable/core/1.0/actions/uninstall.py
+++ unstable/core/1.0/actions/uninstall.py
@@ -1,3 +1,5 @@
 
 def main(j,jp):
+    j.system.fs.createDir("/usr/local/lib/python2.7/dist-packages/")
+    j.system.fs.createDir("/usr/local/lib/python2.7/site-packages/")    
     j.system.platform.python.remove('JumpScale-core')
