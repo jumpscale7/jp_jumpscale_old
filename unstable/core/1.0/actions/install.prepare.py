e4a442e9bf67342cd981d74d3746443a3c7a90b8
despiegk <despiegk@users.noreply.github.com>
2014-03-21 10:42:14 +0100

    python lib mess fix

--- unstable/core/1.0/actions/install.prepare.py
+++ unstable/core/1.0/actions/install.prepare.py
@@ -2,3 +2,7 @@ def main(j,jp):
     j.system.fs.createDir("/usr/local/lib/python2.7/dist-packages/")
     j.system.fs.createDir("/usr/local/lib/python2.7/site-packages/")
     jp.uninstall() 
+    j.system.fs.createDir("/usr/local/lib/python2.7/dist-packages/")
+    j.system.fs.createDir("/usr/local/lib/python2.7/site-packages/")
+
+    
