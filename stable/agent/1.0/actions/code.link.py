23c30885b8a291cb27c7245fbc7d6435cb9d8732
despiegk <despiegk@users.noreply.github.com>
2014-03-30 06:42:44 +0200

    sandbox

--- stable/agent/1.0/actions/code.link.py
+++ stable/agent/1.0/actions/code.link.py
@@ -2,7 +2,8 @@
 #if customizations are required when doing a a linking of the jpackage code to the OS
 
 def main(j,jp,force=True):
-    j.system.fs.removeDirTree("/opt/jumpscale/apps/agent")
+    path="$(jumpscale.paths.base)/apps/agent"
+    j.system.fs.removeDirTree(path)
     recipe=jp.getCodeMgmtRecipe()
     recipe.link(force=force)
 
