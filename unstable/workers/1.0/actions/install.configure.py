e005ad07d80145af238986859d79b499aaf04b8c
Jo De Boeck <deboeck.jo@gmail.com>
2014-04-03 14:07:13 +0200

    No more monitor workers

--- unstable/workers/1.0/actions/install.configure.py
+++ unstable/workers/1.0/actions/install.configure.py
@@ -1,7 +1,2 @@
 def main(j,jp):
-    key = 'workers.queues'
-    if j.application.config.exists(key):
-        workers = j.application.config.getDict(key)
-        if 'monitor' not in workers:
-            workers['monitor'] = 1
-            j.application.config.setDict(key, workers)
+    pass
