83c6e799f00210fd2b1241c522d62d8506aa8d5a
Jo De Boeck <deboeck.jo@gmail.com>
2014-04-08 10:25:38 +0200

    Take unstable into test

--- test/redis/2.8/actions/install.prepare.py
+++ test/redis/2.8/actions/install.prepare.py
@@ -9,19 +9,3 @@ def main(j,jp):
     cmd="killall redis-server"
     j.system.process.execute(cmd,dieOnNonZeroExitCode=False)
 
-    j.system.fs.createDir("/opt/redis/db/redis_cache")
-    j.system.fs.createDir("/opt/redis/db/redis_prod")
-
-    # can happen by e.g. installing a debian package e.g. by
-    ## j.system.platform.ubuntu.install(packagename)
-       
-    #install found debs they need to be in debs dir of one or more of the platforms (all relevant platforms will be used)
-    #args.qp.installUbuntuDebs()
-    
-    #shortcut to some usefull install tools
-    #do=j.system.installtools
-
-    #configuration is not done in this step !!!!!
-    #copying files from files section of jpackages is not done in this step
-    
-    pass
--- test/workers/1.0/actions/install.configure.py
+++ test/workers/1.0/actions/install.configure.py
@@ -1,6 +1,2 @@
 def main(j,jp):
-   
-    #configure the package 
-
     pass
-    
