d9a66682d5dabde6ad4f9e850ba840f2c726bb0b
despiegk <despiegk@users.noreply.github.com>
2014-04-09 08:56:11 +0200

    sandboxing

--- stable/agentcontroller/1.0/actions/code.link.py
+++ stable/agentcontroller/1.0/actions/code.link.py
@@ -2,7 +2,7 @@
 #if customizations are required when doing a a linking of the jpackage code to the OS
 
 def main(j,jp,force=True):
-    j.system.fs.removeDirTree("$(jumpscale.paths.base)/apps/agentcontroller")
+    j.system.fs.removeDirTree("%s/apps/agentcontroller"%j.dirs.baseDir)
     recipe=jp.getCodeMgmtRecipe()
     recipe.link(force=force)
 
--- stable/base/2.7/actions/install.configure.py
+++ stable/base/2.7/actions/install.configure.py
@@ -1,4 +1,4 @@
 
 def main(j,jp):
-    j.application.config.set("jumpscale.paths.base",j.dirs.baseDir)
+    pass
 
