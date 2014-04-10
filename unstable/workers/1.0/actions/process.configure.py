fcc2ac54a7a693afd8b6ee5c3d0e62b1038d4ae5
Jo De Boeck <deboeck.jo@gmail.com>
2014-04-10 15:53:22 +0200

    Make workers take use of new startupmanager feature

--- unstable/workers/1.0/actions/process.configure.py
+++ unstable/workers/1.0/actions/process.configure.py
@@ -6,16 +6,14 @@ def main(j,jp):
     workers = j.application.config.getDict('workers.queues')
     for qname, nrinstances in workers.iteritems():
         print "Queue:%s"% qname
-
-        for nr in range(int(nrinstances)):
-            cmd = 'python'
-            wname="worker_%s_%s"%(qname,nr)
-            if "$(agent.redis.auth)".strip()<>"":
-                args = 'worker.py -pw $(agent.redis.auth) -a $(agent.redis.addr) -p $(agent.redis.port) -qn %s -wn %s'%(qname,wname)
-            else:
-                args = 'worker.py -a $(agent.redis.addr) -p $(agent.redis.port) -qn %s -wn %s'%(qname,wname)
-            workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'agent')
-            j.tools.startupmanager.addProcess(wname, cmd, args=args, env={}, numprocesses=1, priority=21, shell=False, \
-                workingdir=workingdir,jpackage=jp,domain="workers",ports=[])
-            j.tools.startupmanager.startProcess("workers",wname)
+        cmd = 'python'
+        wname="worker_%s"%(qname)
+        if "$(agent.redis.auth)".strip()<>"":
+            args = 'worker.py -pw $(agent.redis.auth) -a $(agent.redis.addr) -p $(agent.redis.port) -qn %s -wn %s'%(qname,wname)
+        else:
+            args = 'worker.py -a $(agent.redis.addr) -p $(agent.redis.port) -qn %s -wn %s'%(qname,wname)
+        workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'agent')
+        j.tools.startupmanager.addProcess(wname, cmd, args=args, env={}, numprocesses=nrinstances, priority=21, shell=False, \
+            workingdir=workingdir,jpackage=jp,domain="workers",ports=[])
+        j.tools.startupmanager.startProcess("workers",wname)
 
