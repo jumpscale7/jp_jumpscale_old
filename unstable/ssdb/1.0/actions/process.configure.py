dd4f2ab6b276be90e3cd7c708570ef7b3756b6bd
kristof <kristof@incubaid.com>
2014-04-13 07:43:27 +0200

    varia

--- unstable/ssdb/1.0/actions/process.configure.py
+++ unstable/ssdb/1.0/actions/process.configure.py
@@ -4,16 +4,62 @@ def main(j,jp):
     
     # jp.log("set autostart $(jp.name)")
 
-    # #example start osis
-    # cmd = 'python'
-    # args = 'osisServerStart.py'
-    # workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'osis')
-    # name = jp.name
-    # domain = jp.domain
-    # ports = jp.ports
-    # j.tools.startupmanager.addProcess(name=name, cmd=cmd, args=args, env={}, numprocesses=1, priority=1, \
-    #    shell=False, workingdir=workingdir,jpackage=jp,domain=domain,ports=ports)
-    
-    #can configure more apps to start than just 1 linked to the jpackage
 
+    j.system.fs.createDir('$vardir/ssdb')
+
+    j.system.fs.remove("$tmpdir/ssdb.pid")
+
+    pd=j.tools.startupmanager.addProcess(\
+        name="ssdb",\
+        cmd="./ssdb-server", \
+        args="$cfgdir/ssdb/ssdb.conf",\
+        env={},\
+        numprocesses=1,\
+        priority=1,\
+        shell=False,\
+        workingdir='$base/apps/ssdb',\
+        jpackage=jp,\
+        domain="serverapps",\
+        ports=[8888],\
+        autostart=True,\
+        reload_signal=0,\
+        user="root",\
+        log=True,\
+        stopcmd=None,\
+        check=True,\
+        timeoutcheck=3,\
+        isJSapp=0,\
+        upstart=False,\
+        stats=True,\
+        processfilterstr="ssdb-server")#what to look for when doing ps ax to find the process
+    
+    pd.start()
+
+    j.dirs.replaceFilesDirVars("$cfgdir/ssdb")
+
+    pd=j.tools.startupmanager.addProcess(\
+        name="webdis",\
+        cmd="./webdis", \
+        args="$cfgdir/ssdb/webdis.json",\
+        env={},\
+        numprocesses=1,\
+        priority=1,\
+        shell=False,\
+        workingdir='$base/apps/ssdb',\
+        jpackage=jp,\
+        domain="serverapps",\
+        ports=[8889],\
+        autostart=True,\
+        reload_signal=0,\
+        user="root",\
+        log=True,\
+        stopcmd=None,\
+        check=True,\
+        timeoutcheck=3,\
+        isJSapp=0,\
+        upstart=False,\
+        stats=True,\
+        processfilterstr="./webdis")#what to look for when doing ps ax to find the process
+    
+    pd.start()    
     pass
