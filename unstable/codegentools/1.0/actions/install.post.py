906fad9e91f38e54c0e13367882e38408cd3b66a
JumpScale Jenkins <jenkins@jumpscale.com>
2014-04-17 12:06:55 +0000

    Moved blobstor

--- unstable/codegentools/1.0/actions/install.post.py
+++ unstable/codegentools/1.0/actions/install.post.py
@@ -1,14 +1,12 @@
 def main(j,jp):
-#     sitepackages = j.application.config.get('python.paths.local.sitepackages')
-#     portalinit = j.system.fs.joinPaths(sitepackages, 'JumpScale', 'portal', '__init__.py')
-#     if not j.system.fs.exists(portalinit):
-#         myinit = """
-# from JumpScale import j
-# import JumpScale.baselib.key_value_store
-# import JumpScale.baselib.taskletengine
-# import JumpScale.baselib.specparser
+     portalinit = j.system.fs.joinPaths(j.dirs.JSlibDir, 'portal', '__init__.py')
+     if not j.system.fs.exists(portalinit):
+         myinit = """
+ from JumpScale import j
+ import JumpScale.baselib.key_value_store
+ import JumpScale.baselib.taskletengine
+ import JumpScale.baselib.specparser
 
-# import codegentools
-# """
-#         j.system.fs.writeFile(portalinit, myinit)
-    pass
+ import codegentools
+ """
+         j.system.fs.writeFile(portalinit, myinit)
