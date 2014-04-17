4b06cd4bbd96d9203e3c39ba70c1df4b0833e1e7
Jo De Boeck <deboeck.jo@gmail.com>
2014-04-17 16:58:26 +0200

    Add libs needed by portal in jpackage

--- unstable/portal/1.0/actions/install.prepare.py
+++ unstable/portal/1.0/actions/install.prepare.py
@@ -2,8 +2,5 @@
 def main(j,jp):
     j.system.fs.removeDirTree("/usr/local/lib/python2.7/site-packages/JumpScale/portal/")
 
-    j.system.platform.ubuntu.install('python-imaging')
     j.system.platform.ubuntu.install('nginx-full')
-    j.system.platform.ubuntu.install('python-beaker')
-    j.system.platform.ubuntu.install('python-mimeparse')
     j.system.platform.ubuntu.install('graphviz')
