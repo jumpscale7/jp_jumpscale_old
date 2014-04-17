bbfa6d634c5eb699c8969a8c549f4080754b2d32
kristof <kristof@incubaid.com>
2014-04-17 05:35:31 +0200

    jssync cmd line changes

--- unstable/redis/2.8/actions/install.prepare.py
+++ unstable/redis/2.8/actions/install.prepare.py
@@ -1,9 +1,5 @@
 def main(j,jp):
    
-    #prepare the platform before copying the files
-
-    j.system.platform.ubuntu.install("python-redis")
-    j.system.platform.ubuntu.checkInstall("redis-server", "redis-server")
 
     # if j.system.net.tcpPortConnectionTest("127.0.0.1",7768)==False:
         # j.clients.redis.deleteInstance("redisp")
