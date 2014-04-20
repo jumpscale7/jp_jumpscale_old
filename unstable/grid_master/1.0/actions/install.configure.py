847cd693a06a7b27028dd67045666d8f9671e235
Jo De Boeck <deboeck.jo@gmail.com>
2014-04-20 16:06:11 +0200

    Update metdata

--- unstable/grid_master/1.0/actions/install.configure.py
+++ unstable/grid_master/1.0/actions/install.configure.py
@@ -5,9 +5,6 @@ def main(j,jp):
     j.application.config.set("grid.id",gridid)
     j.application.config.set("grid.master.ip","127.0.0.1")
 
-    j.packages.findNewest(domain="jumpscale",name="elasticsearch1").install()
-    j.tools.startupmanager.startProcess("jumpscale","elasticsearch")
-
     #check osis installed
     if not j.system.net.tcpPortConnectionTest("127.0.0.1", 5544):
         j.tools.startupmanager.startProcess('jumpscale', 'elasticsearch')
@@ -16,17 +13,12 @@ def main(j,jp):
     if j.application.config.get("grid.master.superadminpasswd") == "":
         passwdmd5 = j.tools.hash.md5_string(j.console.askString("Please provide: grid.master.superadminpasswd", regex="^(?!\s*$).+", retry=5))
         j.application.config.set("grid.master.superadminpasswd",passwdmd5) #on gridmaster we should remember
-    
     passwdmd5 = j.application.config.get("grid.master.superadminpasswd")
 
 
-    j.packages.findNewest(domain="jumpscale",name="libs").install()
-
     redis=j.packages.findNewest(domain="jumpscale",name="redis")
-    redis.install()
     redis.start()
 
-    j.packages.findNewest(domain="jumpscale",name="osis").install()
     j.tools.startupmanager.startProcess("jumpscale","osis")
 
     #register in osis
@@ -38,18 +30,21 @@ def main(j,jp):
     gridobj.initFromLocalNodeInfo()
     key, new, changed = client_grid.set(gridobj)
 
-    j.packages.findNewest(domain="jumpscale",name="grid_portal").install()
-
-    workers = j.packages.findNewest(domain="jumpscale",name="workers")
-    workers.install()
-    workers.start()
 
     j.tools.startupmanager.restartProcess('jumpscale', 'osis') #to make sure we have objects loaded from portal 
 
     #configure avahi
     if j.application.config.getBool("gridmaster.useavahi"):
-        j.application.config.set("grid.useavahi",1)        
+        j.application.config.set("grid.useavahi",1)
         import JumpScale.baselib.remote.avahi
         j.remote.avahi.registerService("js_grid_%s"%gridid,5544)
     else:
         j.application.config.set("grid.useavahi",0)
+
+    # needs to be done in this order after grid_master is registered
+    j.packages.findNewest(domain="jumpscale",name="grid_node").install()
+    j.tools.startupmanager.startProcess("jumpscale","agentcontroller")
+    j.tools.startupmanager.startProcess("workers",None)
+    jp = j.packages.findNewest(domain="jumpscale",name="processmanager")
+    jp.install()
+    jp.start()
