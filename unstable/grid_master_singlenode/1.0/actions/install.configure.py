6922d58da96961a311246b0940123b1b182b0c6b
kristof <kristof@incubaid.com>
2014-04-22 15:11:54 +0200

    repackaging some jpackages, needs testing

--- unstable/grid_master_singlenode/1.0/actions/install.configure.py
+++ unstable/grid_master_singlenode/1.0/actions/install.configure.py
@@ -2,4 +2,6 @@ def main(j,jp):
     j.packages.findNewest('jumpscale', 'core').install()
     j.packages.findNewest('jumpscale', 'elasticsearch1').install()
     j.packages.findNewest('jumpscale', 'graphite').install()
+    j.packages.findNewest('jumpscale', 'codegentools').install()
+    j.packages.findNewest('jumpscale', 'portal').install()
     j.packages.findNewest('jumpscale', 'grid_master').install()
