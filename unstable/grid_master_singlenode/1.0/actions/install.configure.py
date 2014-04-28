81dfd3d3be7e41a9f685eaba0b54c133e5db39a5
Jo De Boeck <deboeck.jo@gmail.com>
2014-04-28 08:07:54 +0200

    Add redis as dependency of grid_master_singlenode

--- unstable/grid_master_singlenode/1.0/actions/install.configure.py
+++ unstable/grid_master_singlenode/1.0/actions/install.configure.py
@@ -1,5 +1,6 @@
 def main(j,jp):
     j.packages.findNewest('jumpscale', 'core').install()
+    j.packages.findNewest('jumpscale', 'redis').install()
     j.packages.findNewest('jumpscale', 'elasticsearch1').install()
     j.packages.findNewest('jumpscale', 'graphite').install()
     j.packages.findNewest('jumpscale', 'codegentools').install()
