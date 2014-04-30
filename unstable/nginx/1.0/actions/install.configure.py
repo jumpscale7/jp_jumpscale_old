e7228444f3a67735c15d6854a2cbe0734adf8e92
Jo De Boeck <deboeck.jo@gmail.com>
2014-04-30 12:02:47 +0200

    Add dependency of nginx for none sandbox

--- unstable/nginx/1.0/actions/install.configure.py
+++ unstable/nginx/1.0/actions/install.configure.py
@@ -5,5 +5,5 @@ def main(j,jp):
     #j.dirs.replaceFilesDirVars("$cfgdir/elasticsearch1")
 
     j.system.fs.createDir("$vardir/log/nginx/")
-    
-    
+    if not j.application.sandbox:
+        j.system.platform.ubuntu.install('liblua5.1-0')
