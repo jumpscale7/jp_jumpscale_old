3308a6d70be16f2f5e671eb9638c0cd5437c5d50
Jo De Boeck <deboeck.jo@gmail.com>
2014-05-06 20:08:08 +0200

    Fix indentation error

--- unstable/codegentools/1.0/actions/install.post.py
+++ unstable/codegentools/1.0/actions/install.post.py
@@ -2,11 +2,11 @@ def main(j,jp):
      portalinit = j.system.fs.joinPaths(j.dirs.jsLibDir, 'portal', '__init__.py')
      if not j.system.fs.exists(portalinit):
          myinit = """
- from JumpScale import j
- import JumpScale.baselib.key_value_store
- import JumpScale.baselib.taskletengine
- import JumpScale.baselib.specparser
+from JumpScale import j
+import JumpScale.baselib.key_value_store
+import JumpScale.baselib.taskletengine
+import JumpScale.baselib.specparser
 
- import codegentools
- """
+import codegentools
+"""
          j.system.fs.writeFile(portalinit, myinit)
