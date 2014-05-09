d0062d9ef747484056df3262ae17466ca5c3ce16
Jo De Boeck <deboeck.jo@gmail.com>
2014-05-09 23:17:27 +0200

    Add javahome to env of elasticsearch

--- unstable/elasticsearch/0.90.5/actions/process.configure.py
+++ unstable/elasticsearch/0.90.5/actions/process.configure.py
@@ -8,7 +8,8 @@ def main(j,jp):
     ymlfile = j.system.fs.joinPaths(j.dirs.cfgDir, 'elasticsearch', 'elasticsearch.yml')
     args="-fD es.config=%s" % ymlfile
     startstoptimeout=60
+    env = {"JAVA_HOME": "$base/apps/openjdk7/"}
     processfilterstr="elasticsearch.yml org.elasticsearch.bootstrap.ElasticSearch"
-    j.tools.startupmanager.addProcess(name, cmd, args=args, domain="jumpscale",jpackage=jp,ports=[9200],priority=1,\
+    j.tools.startupmanager.addProcess(name, cmd, args=args, env=env, domain="jumpscale",jpackage=jp,ports=[9200],priority=1,\
         check=True,timeoutcheck=startstoptimeout,isJSapp=0,processfilterstr=processfilterstr,stats=True,upstart=False)
     # j.tools.startupmanager.startJPackage(jp)
