93d26e0aa5c41a9a0e70001048ec6727863e6957
Jo De Boeck <deboeck.jo@gmail.com>
2014-05-15 19:43:14 +0200

    Set sys.path correct from pth file

--- unstable/core/1.0/actions/install.configure.py
+++ unstable/core/1.0/actions/install.configure.py
@@ -1,2 +1,6 @@
 def main(j,jp):
-    pass
+    if not j.application.sandbox:
+        #this is needed so libraries are accisble before jumpscale is loaded
+        paths = [j.dirs.libDir, j.dirs.libExtDir]
+        pthfile = "/usr/local/lib/python2.7/dist-packages/jumpscale.pth"
+        j.system.fs.writeFile(pthfile, "\n".join(paths))
