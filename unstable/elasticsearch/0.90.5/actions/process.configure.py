ffb786d6c0233c3bed6f30a3038be4d62c943393
Jo De Boeck <deboeckj@codescalers.com>
2014-06-24 09:52:12 +0300

    Fix logger in elasticsearch 0.90.5

--- unstable/elasticsearch/0.90.5/actions/process.configure.py
+++ unstable/elasticsearch/0.90.5/actions/process.configure.py
@@ -3,13 +3,14 @@ def main(j,jp):
     j.system.process.killProcessByPort(9200)
 
     name="elasticsearch"
-    workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'elasticsearch','bin')
-    cmd="%s/elasticsearch"%workingdir
-    ymlfile = j.system.fs.joinPaths(j.dirs.cfgDir, 'elasticsearch', 'elasticsearch.yml')
+    bindir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'elasticsearch','bin')
+    cmd="%s/elasticsearch"% bindir
+    cfgdir = j.system.fs.joinPaths(j.dirs.cfgDir, 'elasticsearch')
+    ymlfile = j.system.fs.joinPaths(cfgdir, 'elasticsearch.yml')
     args="-fD es.config=%s" % ymlfile
     startstoptimeout=60
     env = {"JAVA_HOME": "$base/apps/openjdk7/"}
     processfilterstr="elasticsearch.yml org.elasticsearch.bootstrap.ElasticSearch"
     j.tools.startupmanager.addProcess(name, cmd, args=args, env=env, domain="jumpscale",jpackage=jp,ports=[9200],priority=1,\
-        check=True,timeoutcheck=startstoptimeout,isJSapp=0,processfilterstr=processfilterstr,stats=True,upstart=False)
+        workingdir=cfgdir,check=True,timeoutcheck=startstoptimeout,isJSapp=0,processfilterstr=processfilterstr,stats=True,upstart=False)
     # j.tools.startupmanager.startJPackage(jp)
