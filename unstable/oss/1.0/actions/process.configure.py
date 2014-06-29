bf36cee2d4f99c72edb312fe0aaeb770ecdb35d3
kristof <kristof@incubaid.com>
2014-06-29 14:46:47 +0200

    jpackages youtrack,mongodb

--- unstable/oss/1.0/actions/process.configure.py
+++ unstable/oss/1.0/actions/process.configure.py
@@ -14,29 +14,28 @@ def main(j,jp):
     #timeoutcheck: how long do we wait to see if app active
     #isJSapp: to tell system if process will self register to redis (is jumpscale app)
 
-    # pd=j.tools.startupmanager.addProcess(\
-    #     name=jp.name,\
-    #     cmd="", \
-    #     args="",\
-    #     env={},\
-    #     numprocesses=1,\
-    #     priority=100,\
-    #     shell=False,\
-    #     workingdir='',\
-    #     jpackage=jp,\
-    #     domain=jp.domain,\
-    #     ports=jp.ports,\
-    #     autostart=True,\
-    #     reload_signal=0,\
-    #     user="root",\
-    #     log=True,\
-    #     stopcmd=None,\
-    #     check=True,\
-    #     timeoutcheck=10,\
-    #     isJSapp=1,\
-    #     upstart=False,\
-    #     stats=False,\
-    #     processfilterstr="")#what to look for when doing ps ax to find the process
+    pd=j.tools.startupmanager.addProcess(\
+        name=jp.name,\
+        cmd="python", \
+        args="portal_start.py",\
+        env={},\
+        numprocesses=1,\
+        priority=100,\
+        shell=False,\
+        workingdir='$base/apps/oss',\
+        jpackage=jp,\
+        domain="solutions",\
+        ports=[82],\
+        autostart=True,\
+        reload_signal=0,\
+        user="root",\
+        log=True,\
+        stopcmd=None,\
+        check=True,\
+        timeoutcheck=10,\
+        isJSapp=1,\
+        upstart=False,\
+        stats=False,\
+        processfilterstr="")#what to look for when doing ps ax to find the process
     
-    # pd.start()
-    pass
+    pd.start()
