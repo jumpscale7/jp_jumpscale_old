ba5c1f5f916f099603085c68df8ccb6fb1c6390f
kristof <kristof@incubaid.com>
2014-06-29 13:28:02 +0200

    generic mail robot, example for youtrack

--- unstable/rediskvs/1.0/actions/install.configure.py
+++ unstable/rediskvs/1.0/actions/install.configure.py
@@ -2,8 +2,14 @@ def main(j,jp):
     import JumpScale.baselib.redis
 
     name="rediskvs"
-    slave=(j.application.config.get("rediskvs.master.addr"),j.application.config.get("rediskvs.master.port"),j.application.config.get("rediskvs.secret"))
-    j.clients.redis.configureInstance(name,7771,100,appendonly=False,snapshot=True,slave=slave,ismaster=False)
+    if j.application.config.get("rediskvs.master.addr")=="" or j.application.config.get("rediskvs.master.addr").lower()=="none":
+        slave=False
+        ismaster=True
+    else:
+        slave=(j.application.config.get("rediskvs.master.addr"),j.application.config.get("rediskvs.master.port"),j.application.config.get("rediskvs.secret"))
+        ismaster=False
+    
+    j.clients.redis.configureInstance(name,7771,100,appendonly=False,snapshot=True,slave=slave,ismaster=ismaster)
 
 
 
