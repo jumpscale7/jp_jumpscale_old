1afcd22b5b848476527c0cbca0cd060625a4d9d4
Sherif Zain <sherifzain@yahoo.com>
2014-07-21 15:39:49 +0200

    Moved config from post to configure + using port 80 for now

--- unstable/agentcontroller_nginx/1.0/actions/install.configure.py
+++ unstable/agentcontroller_nginx/1.0/actions/install.configure.py
@@ -1,8 +1,28 @@
 def main(j,jp):
    
-    #configure the package 
-    #j.application.config.applyOnDir("$cfgdir/elasticsearch1")
-    #j.dirs.replaceFilesDirVars("$cfgdir/elasticsearch1")
+    import JumpScale.baselib.startupmanager
 
-    pass
-    
+    osis_ips = j.application.config.getList('osis.ip')
+    osis_servers = ''
+    for ip in osis_ips:
+        osis_servers += '    server %s:5544;\n' % ip
+
+    nginxcfg = '''
+upstream osis {
+    %s
+}
+
+server {
+    listen 80;
+    #ssl on;
+    server_name _;
+    location /osis {
+        proxy_pass  http://osis;
+    }
+}
+    ''' % osis_servers
+    j.system.fs.createDir('$cfgdir/nginx/sites-enabled/')
+    j.system.fs.writeFile('$cfgdir/nginx/sites-enabled/default', nginxcfg)
+    j.tools.startupmanager.restartProcess("serverapps", "nginx")
+
+    return True
--- unstable/agentcontroller_nginx/1.0/actions/install.post.py
+++ unstable/agentcontroller_nginx/1.0/actions/install.post.py
@@ -1,27 +1,2 @@
 def main(j, jp):
-    import JumpScale.baselib.startupmanager
-
-    osis_ips = j.application.config.getList('osis.ip')
-    osis_servers = ''
-    for ip in osis_ips:
-        osis_servers += '    server %s:5544;\n' % ip
-
-    nginxcfg = '''
-upstream osis {
-    %s
-}
-
-server {
-    listen 443;
-    ssl on;
-    server_name _;
-    location /osis {
-        proxy_pass  http://osis;
-    }
-}
-    ''' % osis_servers
-    j.system.fs.createDir('$cfgdir/nginx/sites-enabled/')
-    j.system.fs.writeFile('$cfgdir/nginx/sites-enabled/default', nginxcfg)
-    j.tools.startupmanager.restartProcess("serverapps", "nginx")
-
-    return True
+    pass
