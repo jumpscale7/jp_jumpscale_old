cb6760988eebd047c9d7beadf7a803620696e314
Sherif Zain <sherifzain@yahoo.com>
2014-07-22 09:47:16 +0200

    Added: AC load balancing

--- unstable/grid_loadbalancer/1.0/actions/install.configure.py
+++ unstable/grid_loadbalancer/1.0/actions/install.configure.py
@@ -3,12 +3,24 @@ def main(j,jp):
     import JumpScale.baselib.startupmanager
 
     osis_ips = j.application.config.getList('osis.ip')
+    ac_ips = j.application.config.getList('grid.agentcontroller.ip')
+
     osis_servers = ''
     for ip in osis_ips:
-        osis_servers += '    server %s:5544;\n' % ip
+        osis_servers += 'server %s:5544;\n    ' % ip
+
+    ac_servers = ''
+    for ip in ac_ips:
+        ac_servers += 'server %s:4444;\n    ' % ip
 
     nginxcfg = '''
 upstream osis {
+    ip_hash;
+    %s
+}
+
+upstream ac {
+    ip_hash;
     %s
 }
 
@@ -19,8 +31,11 @@ server {
     location /osis {
         proxy_pass  http://osis;
     }
+    location /ac {
+        proxy_pass  http://ac;
+    }
 }
-    ''' % osis_servers
+    ''' % (osis_servers, ac_servers)
     j.system.fs.createDir('$cfgdir/nginx/sites-enabled/')
     j.system.fs.writeFile('$cfgdir/nginx/sites-enabled/default', nginxcfg)
     j.tools.startupmanager.restartProcess("serverapps", "nginx")
