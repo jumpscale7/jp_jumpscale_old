c0d757f1f8131e4413fc0fd08440d578870f85c3
Sherif Zain <sherifzain@yahoo.com>
2014-07-23 11:09:14 +0200

    Added: pymongo package + modifications in mongodb package

--- unstable/mongodb/1.0/actions/install.prepare.py
+++ unstable/mongodb/1.0/actions/install.prepare.py
@@ -1,14 +1,5 @@
 def main(j,jp):
-   
-    #prepare the platform before copying the files
 
-    # can happen by e.g. installing a debian package e.g. by
-    ## j.system.platform.ubuntu.install(packagename)
-       
-    #install found debs they need to be in debs dir of one or more of the platforms (all relevant platforms will be used)
-    #args.qp.installUbuntuDebs()
-    
-    #shortcut to some usefull install tools
     do=j.system.installtools
 
     print "get mongodb keys added to apt"
@@ -20,28 +11,4 @@ def main(j,jp):
 
     j.system.platform.ubuntu.install("mongodb-org")
 
-    do.execute("killall mongod")
-
-    R="""
-/etc/mongod.conf
-/etc/init/mongod.conf
-/etc/init/mongodb.conf
-/etc/mongodb.conf
-/etc/logrotate.d/mongodb-server
-/etc/init.d/mongod
-/etc/init.d/mongodb
-    """
-    for item in R.split("\n"):
-        j.system.fs.remove(item)
-        
     j.system.fs.createDir("$vardir/mongodb")
-
-    do.execute("apt-get install python-pip -y")
-    do.execute("apt-get install build-essential python-dev -y")
-    do.execute("pip install pymongo")
-
-
-    #configuration is not done in this step !!!!!
-    #copying files from files section of jpackages is not done in this step
-    
-    pass
--- unstable/mongodb/1.0/actions/process.configure.py
+++ unstable/mongodb/1.0/actions/process.configure.py
@@ -16,7 +16,7 @@ def main(j,jp):
 
     pd=j.tools.startupmanager.addProcess(\
         name=jp.name,\
-        cmd="mongod --dbpath $vardir/mongodb --httpinterface --rest", \
+        cmd="mongod --dbpath $vardir/mongodb --smallfiles --rest", \
         args="",\
         env={},\
         numprocesses=1,\
