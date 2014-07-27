1b47507ba10dce8c1191baa4a237e6340b8d4eb0
kristof <kristof@incubaid.com>
2014-07-27 14:23:55 +0200

    jpackage updates & cloudrobot

---
+++ unstable/jsjail/1.0/actions/process.configure.py
@@ -0,0 +1,40 @@
+def main(j,jp):
+   
+    #configure the application to autostart
+    
+    # jp.log("set autostart $(jp.name)")
+
+
+    #numprocesses: if more than 1 process, will be started in tmux as $name_$nr
+    #ports: tcpports
+    #autostart: does this app start auto
+    #stopcmd: if special command to stop
+    #check: check app to see if its running
+    #stats: gather statistics by process manager
+    #timeoutcheck: how long do we wait to see if app active
+    #isJSapp: to tell system if process will self register to redis (is jumpscale app)
+
+    pd=j.tools.startupmanager.addProcess(\
+        name="shellinabox",\
+        cmd="shellinaboxd -s /:root:root:/root:'python $base/apps/jail/shellinabox_launcher.py ${url}' -p $(shellinabox.port) -t --linkify=aggressive", \
+        args="",\
+        env={},\
+        numprocesses=1,\
+        priority=100,\
+        shell=False,\
+        workingdir='$base/apps/jail',\
+        jpackage=jp,\
+        domain=jp.domain,\
+        ports=[$(shellinabox.port)],\
+        autostart=True,\
+        reload_signal=0,\
+        user="root",\
+        log=True,\
+        stopcmd="",\
+        check=True,\
+        timeoutcheck=10,\
+        isJSapp=0,\
+        upstart=False,\
+        stats=False,\
+        processfilterstr="shellinaboxd")#what to look for when doing ps ax to find the process
+
