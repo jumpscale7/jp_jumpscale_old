b4b8897bbd0bf786f3a40865444158e7a756a4d3
Jo De Boeck <deboeck.jo@gmail.com>
2014-08-06 12:01:13 +0300

    Make agentcontroller have hrdinstance to point to its osisconnection

--- unstable/agentcontroller/1.0/actions/process.configure.py
+++ unstable/agentcontroller/1.0/actions/process.configure.py
@@ -2,13 +2,10 @@ def main(j,jp):
     #configure the application to autostart
     # jp.log("set autostart $(jp.name)")
 
-
-    sm=j.tools.startupmanager
-
     cmd = 'python controller.py'
-    args = ''
+    args = '--instance=%s' % jp.instance
     workingdir = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'agentcontroller')
-    name="agentcontroller"
+    name="agentcontroller_%s" % jp.instance
     startstoptimeout=20
     j.tools.startupmanager.addProcess(name, cmd, args=args, env={}, numprocesses=1, priority=20, shell=False, workingdir=workingdir,jpackage=jp,domain="",\
         ports=[4444],check=True,timeoutcheck=startstoptimeout,stats=True,upstart=False)
