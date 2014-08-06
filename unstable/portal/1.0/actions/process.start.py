c5cf6b91e7544eb12ea0459b7ada952ff20f92ef
Jo De Boeck <deboeck.jo@gmail.com>
2014-08-06 11:14:03 +0300

    Make use of osis_client to determine if osis is local

--- unstable/portal/1.0/actions/process.start.py
+++ unstable/portal/1.0/actions/process.start.py
@@ -1,10 +1,13 @@
 def main(j,jp):
-   
     #start the application (only relevant for server apps)
     jp.log("start $(jp.name)")
 
 
-    if jp.hrd_instance.get("portal_auth_source_addr")=="127.0.0.1":
+    osisclientjp = j.packages.findNewest('jumpscale', 'osis_client')
+    osisclientjp.loadActions()
+    osisip = osisclientjp.get('osis.client.addr')
+
+    if j.system.net.isIpLocal(osisip):
         jposis=j.packages.findByName("osis")
         if j.tools.startupmanager.getStatus4JPackage(jposis)==False:
             jposis.start()
