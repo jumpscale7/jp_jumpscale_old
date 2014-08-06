665b7a302bd6c645df80a59b18fa9bf774b7888a
Jo De Boeck <deboeck.jo@gmail.com>
2014-08-06 10:40:46 +0300

    Make osis install grid incase we are not sandbox

--- unstable/osis/1.0/actions/install.prepare.py
+++ unstable/osis/1.0/actions/install.prepare.py
@@ -1,23 +1,12 @@
 def main(j,jp):
-   
-    #prepare the platform before copying the files
-
-    # can happen by e.g. installing a debian package e.g. by
-    ## j.system.platform.ubuntu.install(packagename)
-       
-    #install found debs they need to be in debs dir of one or more of the platforms (all relevant platforms will be used)
-    #args.qp.installUbuntuDebs()
-    
-    #shortcut to some usefull install tools
-    #do=j.system.installtools
-
-    #configuration is not done in this step !!!!!
-    #copying files from files section of jpackages is not done in this step
-    
+    # redis system
     jp2=j.packages.findNewest("jumpscale","redis")
-    jp2.instance="system"    
+    jp2.instance="production"
     if not jp2.isInstalled():
         jp2.install(hrddata={"redis.name":"production","redis.port":"7768","redis.disk":"1","redis.mem":400},instance="production")
     jp2.start()
-
-    
+    # grid lib 
+    if not j.application.sandbox:
+        grid = j.packages.findNewest("jumpscale","grid")
+        if not grid.isInstalled():
+            grid.install()
