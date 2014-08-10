52d947a13bcdb9783a14b47980f05e23d92a2ccc
Jo De Boeck <deboeckj@codescalers.com>
2014-08-10 15:48:59 +0300

    Make sure start knows which instance we are starting

--- unstable/osis/1.0/actions/install.prepare.py
+++ unstable/osis/1.0/actions/install.prepare.py
@@ -3,6 +3,7 @@ def main(j,jp):
     jp2=j.packages.findNewest("jumpscale","redis")
     if not jp2.isInstalled(instance='production'):
         jp2.install(hrddata={"redis.name":"production","redis.port":"7768","redis.disk":"1","redis.mem":400},instance="production")
+    jp2.load(instance='production')
     jp2.start()
     # grid lib 
     if not j.application.sandbox:
