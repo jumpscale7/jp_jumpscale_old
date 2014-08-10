88e34cc54fdbcfeb37550b022a8007617b92245f
Jo De Boeck <deboeckj@codescalers.com>
2014-08-10 16:14:40 +0300

    Put webdis backend redis in webdis jpackage

--- unstable/agentcontroller/1.0/actions/install.prepare.py
+++ unstable/agentcontroller/1.0/actions/install.prepare.py
@@ -3,10 +3,6 @@ def main(j,jp):
     instancename = 'agentcontrolller'
     if not redis.isInstalled(instancename):
         redis.install(hrddata={"redis.name":instancename,"redis.port":"7769","redis.disk":"1","redis.mem":500},instance=instancename)
+    redis.load(instancename)
     redis.start()
 
-    redis=j.packages.findNewest("jumpscale","redis")
-    instancename = 'webdis'
-    if not redis.isInstalled(instancename):
-        redis.install(hrddata={"redis.name":instancename,"redis.port":"7770","redis.disk":"1","redis.mem":300},instance=instancename)
-    redis.start()
