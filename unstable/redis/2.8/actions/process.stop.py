91dd622b0e42c3e03e159e64298797f6ab43a792
Jo De Boeck <deboeckj@codescalers.com>
2014-08-11 15:30:24 +0300

    Dont crash if process not registered yet

--- unstable/redis/2.8/actions/process.stop.py
+++ unstable/redis/2.8/actions/process.stop.py
@@ -1,7 +1,11 @@
 def main(j,jp):
+    from JumpScale.baselib.startupmanager.StartupManager import ProcessNotFoundException
     #stop the application (only relevant for server apps)
     jp.log("stop $(jp.name)")
 
     name="redis_$(redis.name)"
-    j.tools.startupmanager.stopProcess('redis', name)
+    try:
+        j.tools.startupmanager.stopProcess('redis', name)
+    except ProcessNotFoundException:
+        pass
 
